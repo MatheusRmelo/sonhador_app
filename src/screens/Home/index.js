d25e16ce2c25379275175e195ccd5ec7827f1823
matheusRmelo <roberttjmelo@hotmail.com>
2020-11-02 21:19:01 -0300

    Header da Home feito

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import {
-    Container
+    Container,
+    Header,
+    FilterButton,
+    Logo,
+    DirectButton
 } from './styles';
 
-import { Text } from 'react-native';
+import FilterIcon from '../../assets/icons/filter.svg';
+import DirectIcon from '../../assets/icons/direct.svg';
 
 export default () => {
     return( 
         <Container>
-            <Text>SCREEN HOME</Text>
+            <Header>
+                <FilterButton>
+                    <FilterIcon width="32" height="32" fill="white" />
+                </FilterButton>
+                <Logo>SONHADOR</Logo>
+                <DirectButton>
+                    <DirectIcon width="32" height="32" fill="white" />
+                </DirectButton>
+            </Header>
         </Container>
     );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,8 +1,20 @@
 import styled from 'styled-components/native';
+import { colors } from '../../commonStyles';
 const theme = 'LIGHT';
 export const Container = styled.SafeAreaView`
     flex:1;
     background-color: ${theme ? 'white' : 'black'};
-    justify-content: center;
-    align-items: center; 
 `;
+export const Header = styled.View`
+    background-color: ${colors.primary};
+    justify-content: space-between;
+    flex-direction: row;
+    padding: 16px;
+`;
+export const FilterButton = styled.TouchableOpacity``;
+export const Logo = styled.Text`
+    font-size:18px;
+    color: white;
+
+`;
+export const DirectButton = styled.TouchableOpacity``;
